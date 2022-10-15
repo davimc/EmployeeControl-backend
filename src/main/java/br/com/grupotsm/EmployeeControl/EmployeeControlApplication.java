f19d4e2cc189ae4f1d73a299d3984934971b992e
davimc <davimatosc@hotmail.com>
2022-10-15 18:12:01 -0300

    Project created

---
+++ src/main/java/br/com/grupotsm/EmployeeControl/EmployeeControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupotsm.EmployeeControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grupotsm/EmployeeControl/EmployeeControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupotsm.EmployeeControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
