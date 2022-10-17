0b87a8ab7300dfd00574241bcd08a8552f178868
davimc <davimatosc@hotmail.com>
2022-10-16 22:44:12 -0300

    fix: change LicenseType to ReasonType

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,6 +8,6 @@ INSERT INTO tb_employee(name, gender, email, cpf, dt_Admission, birth_Date, stor
 INSERT INTO tb_employee(name, gender, email, cpf, dt_Admission, dt_resignation, birth_Date, store_id) VALUES ('Micaele','F', 'mica@gmail', '000.000.000-00', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '2022-10-1T20:50:07.12345Z',TIMESTAMP WITH TIME ZONE '1989-06-1T20:50:07.12345Z', 4);
 
 
-INSERT INTO tb_license(dt_Start, dt_End, type, employee_id) VALUES (TIMESTAMP '2021-07-13T20:50:07.12345Z', TIMESTAMP '2021-08-13T20:50:07.12345Z', 30, 1);
-INSERT INTO tb_license(dt_Start, dt_End, type, employee_id) VALUES (TIMESTAMP '2021-09-13T20:50:07.12345Z', TIMESTAMP '2021-10-13T20:50:07.12345Z', 30, 2);
-INSERT INTO tb_license(dt_Start, dt_End, type, employee_id) VALUES (TIMESTAMP '2022-10-13T20:50:07.12345Z', TIMESTAMP '2022-10-14T20:50:07.12345Z', 1, 3);
+INSERT INTO tb_license(dt_Start, dt_End, reason, employee_id) VALUES (TIMESTAMP '2021-07-13T20:50:07.12345Z', TIMESTAMP '2021-08-13T20:50:07.12345Z', 30, 1);
+INSERT INTO tb_license(dt_Start, dt_End, reason, employee_id) VALUES (TIMESTAMP '2021-09-13T20:50:07.12345Z', TIMESTAMP '2021-10-13T20:50:07.12345Z', 30, 2);
+INSERT INTO tb_license(dt_Start, dt_End, reason, employee_id) VALUES (TIMESTAMP '2022-10-13T20:50:07.12345Z', TIMESTAMP '2022-10-14T20:50:07.12345Z', 1, 3);
