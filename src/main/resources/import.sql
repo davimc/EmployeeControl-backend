65a7aead64f707d2af7f2ad328bd0004936d40dc
davimc <davimatosc@hotmail.com>
2022-10-16 22:17:15 -0300

    License seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,8 @@ INSERT INTO tb_store(name, type) VALUES ('Olho Dágua',1);
 INSERT INTO tb_employee(name, gender, email, cpf, dt_Admission, birth_Date, store_id) VALUES ('Chiara','F', 'chiara@gmail', '000.000.000-00', TIMESTAMP '2020-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '1989-06-1T20:50:07.12345Z', 2);
 INSERT INTO tb_employee(name, gender, email, cpf, dt_Admission, birth_Date, store_id) VALUES ('Bruno','M', 'bruno@gmail', '000.000.000-00', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '1989-06-1T20:50:07.12345Z', 3);
 INSERT INTO tb_employee(name, gender, email, cpf, dt_Admission, dt_resignation, birth_Date, store_id) VALUES ('Micaele','F', 'mica@gmail', '000.000.000-00', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '2022-10-1T20:50:07.12345Z',TIMESTAMP WITH TIME ZONE '1989-06-1T20:50:07.12345Z', 4);
+
+
+INSERT INTO tb_license(dt_Start, dt_End, type, employee_id) VALUES (TIMESTAMP '2021-07-13T20:50:07.12345Z', TIMESTAMP '2021-08-13T20:50:07.12345Z', 30, 1);
+INSERT INTO tb_license(dt_Start, dt_End, type, employee_id) VALUES (TIMESTAMP '2021-09-13T20:50:07.12345Z', TIMESTAMP '2021-10-13T20:50:07.12345Z', 30, 2);
+INSERT INTO tb_license(dt_Start, dt_End, type, employee_id) VALUES (TIMESTAMP '2022-10-13T20:50:07.12345Z', TIMESTAMP '2022-10-14T20:50:07.12345Z', 1, 3);
