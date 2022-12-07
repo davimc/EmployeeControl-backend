0999ab573250750a8cf05952093d36e760784b99
davimc <davimatosc@hotmail.com>
2022-12-07 10:04:23 -0300

    Exception handler

---
+++ src/main/java/br/com/grupotsm/EmployeeControl/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package br.com.grupotsm.EmployeeControl.resources.exceptions;
+
+import br.com.grupotsm.EmployeeControl.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError();
+        err.setTimeStamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Resource not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
