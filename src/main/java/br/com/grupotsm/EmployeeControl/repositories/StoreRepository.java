4ee905c713b637d860521296196cb65bbfc7b101
davimc <davimatosc@hotmail.com>
2022-12-07 10:03:04 -0300

    cleaning imports

--- src/main/java/br/com/grupotsm/EmployeeControl/repositories/StoreRepository.java
+++ src/main/java/br/com/grupotsm/EmployeeControl/repositories/StoreRepository.java
@@ -1,8 +1,12 @@
 package br.com.grupotsm.EmployeeControl.repositories;
 
+import br.com.grupotsm.EmployeeControl.DTO.store.StoreExpandedDTO;
 import br.com.grupotsm.EmployeeControl.entities.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
