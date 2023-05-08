35074babac0cbd5f9b8ea611107a90bbaa5889cc
davimc <davimatosc@hotmail.com>
2023-05-08 15:53:50 -0300

    Releasing access to cors

--- src/main/java/br/com/grupotsm/EmployeeControl/config/CorsConfiguration.java
+++ src/main/java/br/com/grupotsm/EmployeeControl/config/CorsConfiguration.java
@@ -9,7 +9,6 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")
                 .allowedOrigins("https://emplocontroll.netlify.app/")
                 .allowedMethods("GET","POST","PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
