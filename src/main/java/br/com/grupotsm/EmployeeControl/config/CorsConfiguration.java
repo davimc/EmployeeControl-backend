d8652328d0ce77210d49d7e222be0508610948f3
davimc <davimatosc@hotmail.com>
2023-06-05 09:11:02 -0300

    Testing cors

--- src/main/java/br/com/grupotsm/EmployeeControl/config/CorsConfiguration.java
+++ src/main/java/br/com/grupotsm/EmployeeControl/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://emplocontroll.netlify.app/","http://localhost:5173/")
+                .allowedOrigins("https://emplocontroll.netlify.app/")
                 .allowedMethods("GET","POST","PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
