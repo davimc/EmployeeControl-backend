485ef02e2ea28a5f9c2fc0ca29389b66b30a3999
davimc <davimatosc@hotmail.com>
2023-06-06 09:35:53 -0300

    Handling validationError

--- src/main/java/br/com/grupotsm/EmployeeControl/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/grupotsm/EmployeeControl/resources/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.grupotsm.EmployeeControl.resources.exceptions;
 import br.com.grupotsm.EmployeeControl.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,6 +22,33 @@ public class ResourceExceptionHandler {
         err.setError("Resource not found");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<StandardError> illegalArgument(IllegalArgumentException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError();
+        err.setTimeStamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Illegal Argument");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        ValidationError err = new ValidationError();
+        err.setTimeStamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Validation exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        e.getBindingResult().getFieldErrors().stream()
+                .forEach(f -> err.addError(f.getField(),f.getDefaultMessage()));
 
         return ResponseEntity.status(status).body(err);
     }
